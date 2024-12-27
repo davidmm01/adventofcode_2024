3b41e9cb107f907a808587f0f7051e178b49a7f5
David Michieli <d.m.michieli@gmail.com>
2024-12-27 14:32:33 +1100

    finally finish day 3 lol

--- days/day_3/main.go
+++ days/day_3/main.go
@@ -12,8 +12,16 @@ import (
 func main() {
 	fmt.Println("Day 3")
 
-	mulRegex := regexp.MustCompile(`mul\([[\d]{1,3},[\d]{1,3}\)`)
-	digitPairRegex := regexp.MustCompile(`[\d]{1,3}`)
+	digitPairRegexStr := `[\d]{1,3}`
+	mulRegexStr := fmt.Sprintf(`mul\(%s,%s\)`, digitPairRegexStr, digitPairRegexStr)
+	doRegexStr := `do\(\)`
+	dontRegexStr := `don't\(\)`
+
+	mulRegex := regexp.MustCompile(mulRegexStr)
+	digitPairRegex := regexp.MustCompile(digitPairRegexStr)
+	doRegex := regexp.MustCompile(doRegexStr)
+	dontRegex := regexp.MustCompile(dontRegexStr)
+	regexForAllInstructions := regexp.MustCompile(fmt.Sprintf(`(%s)|(%s)|(%s)`, mulRegexStr, doRegex, dontRegex))
 
 	// read input
 	readFile, err := os.Open("days/day_3/input.txt")
@@ -43,4 +51,50 @@ func main() {
 	readFile.Close()
 
 	fmt.Println("Part 1:", sum)
+
+	readFile, err = os.Open("days/day_3/input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner = bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	sum = 0
+	enabled := true
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+
+		instructions := regexForAllInstructions.FindAll([]byte(line), -1)
+		for _, instruction := range instructions {
+			dont := dontRegex.Match(instruction)
+			if dont {
+				enabled = false
+				continue
+			}
+
+			do := doRegex.Match(instruction)
+			if do {
+				enabled = true
+				continue
+			}
+
+			mul := mulRegex.Match(instruction)
+			if mul {
+				if enabled {
+					digits := digitPairRegex.FindAll(instruction, 2)
+					digit1, _ := strconv.Atoi(string(digits[0]))
+					digit2, _ := strconv.Atoi(string(digits[1]))
+					sum += digit1 * digit2
+				}
+				continue
+			}
+
+			panic("blowing up:" + string(instruction))
+		}
+	}
+	readFile.Close()
+
+	fmt.Println("Part 2:", sum)
 }
