6391c48775b12fd08c8e762419d4d12022df2f58
David Michieli <d.m.michieli@gmail.com>
2025-01-01 12:11:56 +1100

    finish day 4

--- days/day_4/main.go
+++ days/day_4/main.go
@@ -34,9 +34,7 @@ func main() {
 	xMax := len(wordSearch[0])
 	yMax := len(wordSearch)
 
-	fmt.Println(xMax)
-	fmt.Println(yMax)
-
+	// part1
 	total := 0
 	for y, wordSearchLine := range wordSearch {
 		for x, char := range wordSearchLine {
@@ -48,31 +46,82 @@ func main() {
 					XMax:       xMax,
 					YMax:       yMax,
 				}
-				total += checkAll(params)
+				total += checkAllP1(params)
 			}
 		}
 	}
+	fmt.Println("Part 1:", total)
 
+	// part2
+	total = 0
+	for y, wordSearchLine := range wordSearch {
+		for x, char := range wordSearchLine {
+			if char == "A" {
+				params := checkParams{
+					WordSearch: wordSearch,
+					X:          x,
+					Y:          y,
+					XMax:       xMax,
+					YMax:       yMax,
+				}
+				total += checkAllP2(params)
+			}
+		}
+	}
+	fmt.Println("Part 2:", total)
 }
 
-// checkAll searches for the word XMAS in all directions.
+func checkAllP2(params checkParams) int {
+	if params.WordSearch[params.Y][params.X] != "A" {
+		panic("Can only check for words when starting on 'A'")
+	}
+
+	if params.Y-1 < 0 || params.Y+1 >= params.YMax || params.X-1 < 0 || params.X+1 >= params.XMax {
+		return 0
+	}
+
+	topLeftToBottomRight := (params.WordSearch[params.Y-1][params.X-1] == "M" && params.WordSearch[params.Y+1][params.X+1] == "S") ||
+		(params.WordSearch[params.Y-1][params.X-1] == "S" && params.WordSearch[params.Y+1][params.X+1] == "M")
+
+	bottomLeftToTopRight := (params.WordSearch[params.Y+1][params.X-1] == "M" && params.WordSearch[params.Y-1][params.X+1] == "S") ||
+		(params.WordSearch[params.Y+1][params.X-1] == "S" && params.WordSearch[params.Y-1][params.X+1] == "M")
+
+	if topLeftToBottomRight && bottomLeftToTopRight {
+		return 1
+	}
+
+	return 0
+}
+
+type checkParams struct {
+	WordSearch [][]string
+	X          int
+	Y          int
+	XMax       int
+	YMax       int
+}
+
+// checkAllP1 searches for the word XMAS in all directions.
 // Must only be called when
-func checkAll(params checkParams) int {
+func checkAllP1(params checkParams) int {
 	if params.WordSearch[params.Y][params.X] != "X" {
 		panic("Can only check for words when starting on 'X'")
 	}
 
 	sum := 0
-	sum += checkUp(params)
-	sum += checkDown(params)
-	sum += checkLeft(params)
-	sum += checkRight(params)
-	sum += checkUpLeftDiag(params)
-	sum += checkDownRightDiag(params)
+	sum += checkUpP1(params)
+	sum += checkDownP2(params)
+	sum += checkLeftP1(params)
+	sum += checkRightP1(params)
+	sum += checkUpLeftDiagP1(params)
+	sum += checkUpRightDiagP1(params)
+	sum += checkDownLeftDiagP1(params)
+	sum += checkDownRightDiagP1(params)
+
 	return sum
 }
 
-func checkUp(params checkParams) int {
+func checkUpP1(params checkParams) int {
 	// to check up, require 3 rows above this one to exist
 	if params.Y-3 < 0 {
 		return 0
@@ -87,9 +136,9 @@ func checkUp(params checkParams) int {
 	return 0
 }
 
-func checkDown(params checkParams) int {
+func checkDownP2(params checkParams) int {
 	// to check down, require 3 rows below this one to exist
-	if params.Y+3 < params.YMax {
+	if params.Y+3 >= params.YMax {
 		return 0
 	}
 
@@ -102,34 +151,89 @@ func checkDown(params checkParams) int {
 	return 0
 }
 
-func checkLeft(params checkParams) int {
+func checkLeftP1(params checkParams) int {
+	// to check left, require 3 columns to the left to exist
+	if params.X-3 < 0 {
+		return 0
+	}
+
+	if params.WordSearch[params.Y][params.X-1] == "M" &&
+		params.WordSearch[params.Y][params.X-2] == "A" &&
+		params.WordSearch[params.Y][params.X-3] == "S" {
+		return 1
+	}
+
 	return 0
 }
 
-func checkRight(params checkParams) int {
+func checkRightP1(params checkParams) int {
+	// to check right, require 3 columns to the left to exist
+	if params.X+3 >= params.XMax {
+		return 0
+	}
+
+	if params.WordSearch[params.Y][params.X+1] == "M" &&
+		params.WordSearch[params.Y][params.X+2] == "A" &&
+		params.WordSearch[params.Y][params.X+3] == "S" {
+		return 1
+	}
+
 	return 0
 }
 
-func checkUpLeftDiag(params checkParams) int {
+func checkUpLeftDiagP1(params checkParams) int {
+	// to check up left diag, require 3 up and 3 left
+	if params.Y-3 < 0 || params.X-3 < 0 {
+		return 0
+	}
+
+	if params.WordSearch[params.Y-1][params.X-1] == "M" &&
+		params.WordSearch[params.Y-2][params.X-2] == "A" &&
+		params.WordSearch[params.Y-3][params.X-3] == "S" {
+		return 1
+	}
+
 	return 0
 }
 
-func checkUpRightDiag(params checkParams) int {
+func checkUpRightDiagP1(params checkParams) int {
+	if params.Y-3 < 0 || params.X+3 >= params.XMax {
+		return 0
+	}
+
+	if params.WordSearch[params.Y-1][params.X+1] == "M" &&
+		params.WordSearch[params.Y-2][params.X+2] == "A" &&
+		params.WordSearch[params.Y-3][params.X+3] == "S" {
+		return 1
+	}
+
 	return 0
 }
 
-func checkDownLeftDiag(params checkParams) int {
+func checkDownLeftDiagP1(params checkParams) int {
+	if params.Y+3 >= params.YMax || params.X-3 < 0 {
+		return 0
+	}
+
+	if params.WordSearch[params.Y+1][params.X-1] == "M" &&
+		params.WordSearch[params.Y+2][params.X-2] == "A" &&
+		params.WordSearch[params.Y+3][params.X-3] == "S" {
+		return 1
+	}
+
 	return 0
 }
 
-func checkDownRightDiag(params checkParams) int {
-	return 0
-}
+func checkDownRightDiagP1(params checkParams) int {
+	if params.Y+3 >= params.YMax || params.X+3 >= params.XMax {
+		return 0
+	}
 
-type checkParams struct {
-	WordSearch [][]string
-	X          int
-	Y          int
-	XMax       int
-	YMax       int
+	if params.WordSearch[params.Y+1][params.X+1] == "M" &&
+		params.WordSearch[params.Y+2][params.X+2] == "A" &&
+		params.WordSearch[params.Y+3][params.X+3] == "S" {
+		return 1
+	}
+
+	return 0
 }
